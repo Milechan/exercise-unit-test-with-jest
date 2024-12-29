5c9f9641f1bdf4a02d885aa269b2e2b7570c5250
milena <milena.concha.m@gmail.com>
2024-12-29 15:39:04 -0300

    exportar modulo tasaCambio

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function dollarToYen(dollars) {
-    let result = dollars *157.82;
-    return result 
-    
+    let result = dollars * 157.82;
+    return result
 }
 function euroToDollar(euro) {
-    let result = euro *1.04;
+    let result = euro * 1.04;
     return result
 }
 
-function YenToPound (yen){
-    let result = yen *0.0050
+function yenToPound(yen) {
+    let result = yen * 0.0050
     return result
 }
+
+module.exports = { dollarToYen, euroToDollar, yenToPound }
