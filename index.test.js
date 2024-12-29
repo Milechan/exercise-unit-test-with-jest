d5d26339094d18e8779524481b1440cb986524ab
milena <milena.concha.m@gmail.com>
2024-12-29 15:39:37 -0300

    importar modulo tasaCambio

--- index.test.js
+++ index.test.js
@@ -1,18 +1,21 @@
-test ("100 dolares son 15782.50 yenes",() =>{
-    let result = dollarToYen(100);
-    expect(result).toBe(15782.50);
+const tasaCambio = require("./index");
+
+
+test("100 dolares son 15782.50 yenes", () => {
+    let result = tasaCambio.dollarToYen(100);
+    expect(result).toBe(15782);
 })
 
-test ("100 euros son 104.24 dolares",() =>{
-let result = euroToDollar(100);
-expect(result).toBe(104.24);
+test("100 euros son 104.24 dolares", () => {
+    let result = tasaCambio.euroToDollar(100);
+    expect(result).toBe(104);
 })
 
-test ("100 yesnes son 0.50 libras esterlians",() =>{
-let result = YenToPound(100)
-expect (result).toBe(0.50);
+test("100 yesnes son 0.50 libras esterlians", () => {
+    let result = tasaCambio.yenToPound(100)
+    expect(result).toBe(0.50);
 })
-test ("50 euros son 52.12 dolares",() =>{
-    let result = euroToDollar(50);
-    expect(result).toBe(52.12);
-    })
+test("50 euros son 52.12 dolares", () => {
+    let result = tasaCambio.euroToDollar(50);
+    expect(result).toBe(52);
+})
